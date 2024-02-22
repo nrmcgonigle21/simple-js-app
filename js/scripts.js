763c44361777393bf9d285ec9eac4f85ca7b94fb
nrmcgonigle21 <nrmcgonigle21@gmail.com>
2024-02-22 15:12:13 -0500

    Created a array for the var Pokemonlist
    Created an array for the variable pokemonlist. Added 3 objects to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 var pokemonlist = []
+pokemonlist.push(
+    { name: "pikachu", height: 4, types: ['electric'] },
+    { name: "Charizard", height: 5, types: ['fire', 'flying'] },
+    { name: "Bulbasaur", height: 3, types: ['grass', 'poison'] }
+);
