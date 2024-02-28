b4f0c49094f02fc0d9fa412d859f2c5c145b7196
nrmcgonigle21 <nrmcgonigle21@gmail.com>
2024-02-28 15:35:05 -0500

    Created a for loop
    Created a "for loop" that iterates the pokemon names in the array and prints the name on the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-var pokemonlist = []
-pokemonlist.push(
+var pokemonList = []
+pokemonList.push(
     { name: "pikachu", height: 4, types: ['electric'] },
     { name: "Charizard", height: 5, types: ['fire', 'flying'] },
     { name: "Bulbasaur", height: 3, types: ['grass', 'poison'] }
 );
+//pokemon list array
+for (let i=0; i < pokemonList.length; i++) {
+    document.write(`<h2>${pokemonList[i].name}</h2> <p>${pokemonList[i].height}</p>`)
+}
+//loop that iterates the pokemon in the array and prints a list of pokemon names
