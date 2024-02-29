d6f26facd1971851af95d824f88c761b50bac98a
nrmcgonigle21 <nrmcgonigle21@gmail.com>
2024-02-29 11:04:13 -0500

    Changes a comment

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ pokemonList.push(
     { name: "Charizard", height: 5, types: ['fire', 'flying'] },
     { name: "Bulbasaur", height: 3, types: ['grass', 'poison'] }
 );
-//pokemon list array
+//loop through the list array
 for (let i=0; i < pokemonList.length; i++) {
     document.write(`<h2>${pokemonList[i].name}</h2> <p>${pokemonList[i].height}</p>`)
 }
-//loop that iterates the pokemon in the array and prints a list of pokemon names
+
