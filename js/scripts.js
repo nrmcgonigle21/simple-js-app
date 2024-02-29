766f848648ca19190a87cf07fb1ae334ba687a8c
nrmcgonigle21 <nrmcgonigle21@gmail.com>
2024-02-29 12:53:44 -0500

    declared a variable
    declared the variable tallest height

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,23 @@ pokemonList.push(
     { name: "pikachu", height: 4, types: ['electric'] },
     { name: "Charizard", height: 5, types: ['fire', 'flying'] },
     { name: "Bulbasaur", height: 3, types: ['grass', 'poison'] }
+    //array of pokemon names, including their height and abilities
 );
-//loop through the list array
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(`<span><strong class="pokename">${pokemonList[i].name}</strong> (${pokemonList[i].height})</span><br>`)
+//find the tallest pokemon
+let tallestHeight = 0;
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > tallestHeight) {
+        tallestHeight = pokemonList[i].height;
+    }
 }
+
+//loop through the list array
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height === tallestHeight) {
+        document.write(`<span><strong class="pokename">${pokemonList[i].name} - Wow, that's big!</strong> (${pokemonList[i].height})</span><br>`);
+    } else {
+        document.write(`<span><strong class="pokename">${pokemonList[i].name}</strong> (${pokemonList[i].height})</span><br>`);
+    }
+}
+
 
